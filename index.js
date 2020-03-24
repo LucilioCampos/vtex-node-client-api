c81f77718fc33279a593c909986dd361beb4cb93
lucilio <lucilio@codeby.com.br>
2020-03-24 13:53:42 -0300

    include all endpoints for skus

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const {Client} = require('./lib/client')
-const axios = require('axios')
-
-const vtex = Client({ accountName: 'tste', appKey: "", appToken: "", environment:""})
-module.exports = Client
+module.exports = require('./lib/client')
