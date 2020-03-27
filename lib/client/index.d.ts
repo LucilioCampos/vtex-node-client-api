2c5d329d8a5d1075f8ed374f8eb872bdea7300b2
lucilio <lucilio@codeby.com.br>
2020-03-26 23:55:46 -0300

    add skus to typescript

--- lib/client/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface IClient {
-    accountName: string,
-    appKey?: string,
-    appToken?: string,
-    autToken?: string,
-    enviroment: string
-
-}
-export declare const Client: (client: IClient) => void;
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export const Client = (name: string) => "ola";
